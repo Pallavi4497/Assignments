56528db9c56ddedcf5eb1315a42a575a23f6ba84
Admin <Admin@DELL>
2020-10-22 17:53:42 +0530

    First commit

---
+++ Selenium&RestAssignments/src/test/java/SeleniumClient/NewTest2.java
@@ -0,0 +1,21 @@
+package SeleniumClient;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest2 {
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
